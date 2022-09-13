85aeb892879c3d51386bfc3113d9e468bb3f6a69
Enofua Divine <divineenofua@gmail.com>
2022-09-12 20:01:20 -1200

    latest commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var myArray = [];
-var people = ["Terry", "Henry", "Larry", 'kim', "Joanne", "Lisa", "Jane"];
+var people = ["Terry", "Jerry", "Ruona", "Divine", "Great", "Deborah", "Henry", "Larry", 'kim', "Joanne", "Lisa", "Jane"];
 var key;  
+var box = document.querySelector('.box');
 function start(){
     myArray = people.slice();
     myArray.sort(function(a,b){
@@ -8,4 +9,19 @@ function start(){
     });
     console.log(myArray);
     build(); 
+}
+function build() {
+    var html = "<h1 class='header'>Name Game</h1>";
+    for(var x= 0; x<myArray.length; x++){
+        var str = myArray[x];
+        var shuffled = str.split("").sort(function () {
+            return 0.5 - Math.random()
+        }).join("");
+        html += '<div class="box" onmouseover="uText(this,\"+shuffled+\")" onmouseout="uText(this,\"Hidden "+(x+1)\")">Hidden '+ (x+1)+'<br>'+shuffled+'</div>';
+    }
+    document.getElementById('output').innerHTML = html;
+
+}
+function uText(t,mes){
+    t.innerHTML = mes;
 }
